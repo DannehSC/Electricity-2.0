33d08c68a1dba98015a35a90191432d04fcc8b87
Lego MM <dangerspookycanyon@gmail.com>
2017-05-23 15:22:56 -0500

    fourth

--- Modules/Commands.lua
+++ Modules/Commands.lua
@@ -1,4 +1,4 @@
 Commands={}
 function addCommand(name,desc,cmds,rank,multi_arg,func)
-
+	Commands[name]={Name=name,Description=desc,Commands=(type(cmds)=='table'and cmds or{cmds}),Rank=rank,Multi=multi_arg,Function=func}
 end
