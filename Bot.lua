8dce1288d566a81f7b2b5fb9172f96e46a16a4d4
Lego MM <dangerspookycanyon@gmail.com>
2017-05-23 16:00:06 -0500

    sixth

--- Bot.lua
+++ Bot.lua
@@ -1,26 +1,44 @@
 local token=require('./token')
+hooks=require('./hooks')
 timer=require('timer')
 fs=require('fs')
 discordia=require('discordia')
 client=discordia.Client({fetchMembers=true})
+uptime=discordia.Stopwatch()
+function FFB(t)--format for beta
+	if beta==true then
+		return t..'_B'
+	else
+		return t
+	end
+end
 function loadModule(name)
 	name=name..'.lua'
 	local data,others=fs.readFileSync('Modules/'..name)
 	if data then
 		local a,b=loadstring(data)
-		if not a then 
+		if not a then
 			print("<SYNTAX> ERROR LOADING "..name.."\nERROR:"..b)
+			if sendLog then
+				sendLog(hooks[FFB('Errors')],"MODULE SYNTAX",string.format("MODULE NAME: %s\nERROR: %s",name,tostring(b)))
+			end
 			return false
 		else
 			setfenv(a,getfenv())
 			local c,d=pcall(a)
 			if not c then
 				print("<RUNTIME> ERROR LOADING "..name.."\nERROR:"..d)
+				if sendLog then
+					sendLog(hooks[FFB('Errors')],"MODULE RUNTIME",string.format("MODULE NAME: %s\nERROR: %s",name,tostring(d)))
+				end
 				return false
 			end
 		end
 	else
 		print("<LOADING DATA> ERROR LOADING "..name.."\nERROR:"..tostring(data),tostring(others))
+		if sendLog then
+			sendLog(hooks[FFB('Errors')],"MODULE LOADING",string.format("MODULE NAME: %s\nERROR: %s",name,tostring(data)..'\n'..tostring(others)))
+		end
 		return false
 	end
 end
--- Modules/Functions.lua
+++ Modules/Functions.lua
@@ -1,3 +1,19 @@
+http=require("coro-http")
+json=require("json")
 function timeStamp()
 	return os.date("%I:%M:%S %p - %a, %b %d")
+end
+function sendLog(hook,title,description,color)
+	if not hook then print"NO HOOK"return end
+	local post
+	if type(title)=='table'then
+		title.color=color
+		post={embeds={title}}
+	else
+		post={embeds={{title=title,description=description,color=color}}}
+	end
+	local a,b=pcall(coroutine.wrap(function()
+		http.request("POST",hook,{{"Content-Type","application/json"}},json.encode(post))
+	end))
+	print(a,b)
 end
