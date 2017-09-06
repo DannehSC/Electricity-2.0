61add90f8e7ccc7c66474e1be43236ec53dba46c
Jacob Parker <2037716+monofur@users.noreply.github.com>
2017-09-05 22:09:06 -0700

    fixing ur noobness
    fixed

--- bot.sh
+++ bot.sh
@@ -1,16 +1,3 @@
-git clone https://github.com/DannehSC/Electricity-2.0.git
-rm -rf /bot
-mv /Electricity-2.0 /bot
-cp /luvit/luvit /bot
-cp /luvit/luvi /bot
-cp /luvit/lit /bot
-cp /luvit/stuff.zip /bot
-cd /bot
-unzip stuff.zip
-chmod a+x luvit
-chmod a+x luvi
-chmod a+x luv
-rm -rf deps
 git clone https://github.com/DannehSC/Electricity-2.0-Deps.git
 mv Electricity-2.0-Deps deps
 rm -rf /Electricity-2.0
